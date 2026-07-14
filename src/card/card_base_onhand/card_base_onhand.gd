70adb07848c17628ccb80aba7d5a2508e676648d
Lz-sx <1927720883@qq.com>
2026-07-14 20:36:31 +0800

    手牌完成

--- src/card/card_base_onhand/card_base_onhand.gd
+++ src/card/card_base_onhand/card_base_onhand.gd
@@ -1,7 +1,7 @@
 extends Control
 class_name CardBaseOnhand
 
-@onready var texture_button: Panel = $TextureButton
+@onready var texture_button: TextureButton = $TextureButton
 
 var tween:Tween
 var is_ready:bool = false
@@ -10,7 +10,8 @@ func _ready() -> void:
 	await get_tree().create_timer(0.2).timeout
 	is_ready = true
 
-func _on_mouse_entered() -> void:
+func _on_texture_button_mouse_entered() -> void:
+	print("yes")
 	if not is_ready:
 		return
 	if tween:
@@ -18,7 +19,9 @@ func _on_mouse_entered() -> void:
 	tween = create_tween()
 	tween.tween_property(texture_button,"position",Vector2(0,-8),0.1)
 
-func _on_mouse_exited() -> void:
+
+func _on_texture_button_mouse_exited() -> void:
+	print("no")
 	if tween:
 		tween.kill()
 	tween = create_tween()
