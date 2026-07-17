6b5e8830377636044f526b895f7ffdb5accafd35
Lz-sx <1927720883@qq.com>
2026-07-17 14:14:58 +0800

    交互功能

--- src/core/occupancy.gd
+++ src/core/occupancy.gd
@@ -23,35 +23,46 @@ func occupy_unit(selected_unit:CardBaseOnmap,target_unit:CardBaseOnmap) -> int:
 			if target_unit.capacity > 0 and selected_unit.Type == Data.Type.CHARACTER:
 				remove_node(selected_unit)
 				target_unit.get_node("Passenger").add_child(selected_unit)
+				target_unit.capacity-=1
 				return 0
 		elif target_unit.Type == Data.Type.BUILDING:
 				target_unit = target_unit as BuildingCardBase
-				if target_unit.capacity > 0 and selected_unit.Type != Data.Type.BUILDING:
+				if target_unit.capacity > 0 and selected_unit.Type == Data.Type.VEHICLE:
 					remove_node(selected_unit)
 					target_unit.get_node("Garrison").add_child(selected_unit)
+					target_unit.capacity-=1
 					return 1
+				if target_unit.capacity > 0 and selected_unit.Type == Data.Type.CHARACTER:
+					remove_node(selected_unit)
+					target_unit.get_node("Garrison").add_child(selected_unit)
+					target_unit.capacity-=1
+					return 2
 				if target_unit.capacity==0:
 					var child:CardBaseOnmap = target_unit.get_node("Garrison").get_child(0)
 					if child.Type == Data.Type.CHARACTER and selected_unit.Type == Data.Type.VEHICLE:
+						selected_unit = selected_unit as VehicleCardBase
 						remove_node(selected_unit)
 						remove_node(child)
 						selected_unit.get_node("Passenger").add_child(child)
+						selected_unit.capacity-=1
 						target_unit.get_node("Garrison").add_child(selected_unit)
-						return 2
+						return 3
 					child = child as VehicleCardBase
 					if child.Type==Data.Type.VEHICLE and child.capacity > 0 and selected_unit.Type==Data.Type.CHARACTER:
 						remove_node(selected_unit)
 						child.get_node("Passenger").add_child(child)
-						return 3
+						child.capacity-=1
+						return 4
 	return -1
 			
 func vacate_unit(selected_unit:CardBaseOnmap,tile_position:Vector2i):
 	if selected_unit.Type!=Data.Type.BUILDING:
 		if selected_unit.get_parent() != null:
+			selected_unit.get_parent().get_parent().capacity+=1
 			selected_unit.get_parent().remove_child(selected_unit)
 		unit_spawner.container.add_child(selected_unit)
 
-func occupy_move(unit:CardBaseOnmap,position:Vector2i,rotation:int):
+func rotate(unit:CardBaseOnmap,position:Vector2i,rotation:int):
 	var tween:Tween = create_tween()
 	# 同时平滑移动位置 + 旋转
 	tween.tween_property(unit, "position", position, 0.4)
@@ -60,16 +71,35 @@ func occupy_move(unit:CardBaseOnmap,position:Vector2i,rotation:int):
 	tween.set_trans(Tween.TRANS_SINE)
 	tween.set_ease(Tween.EASE_OUT)
 
-
 func occupy(selected_unit:CardBaseOnmap,tile_position:Vector2i):
 	var target_unit = game_grid.get_cell_data(tile_position)
 	movement.move(selected_unit,tile_position)
 	match occupy_unit(selected_unit,target_unit):
 		0:
-			occupy_move(selected_unit,OCCUPY_OFFSET_RIGHT,ROTATION_RIGHT)
+			rotate(selected_unit,OCCUPY_OFFSET_RIGHT,ROTATION_RIGHT)
+			rotate(target_unit,OCCUPY_OFFSET_LEFT,ROTATION_LEFT)
 		1:
-			pass
-			
+			selected_unit = selected_unit as VehicleCardBase
+			if selected_unit.capacity == Data.card_data[selected_unit.id]["capacity"]:
+				rotate(selected_unit,OCCUPY_OFFSET_LEFT,ROTATION_LEFT)
+		2:
+			rotate(selected_unit,OCCUPY_OFFSET_RIGHT,ROTATION_RIGHT)
+		3:
+			rotate(selected_unit,OCCUPY_OFFSET_LEFT,ROTATION_LEFT)
+		4:
+			rotate(selected_unit,OCCUPY_OFFSET_RIGHT,ROTATION_RIGHT)
+		_:
+			push_warning("错误：occupy函数判断链")
+	
+func vacate(selected_unit:CardBaseOnmap,tile_position:Vector2i):
+	if selected_unit.Type == Data.Type.CHARACTER:
+		rotate(selected_unit,OCCUPY_OFFSET_LEFT,ROTATION_LEFT)
+	elif selected_unit.Type == Data.Type.VEHICLE:
+		selected_unit = selected_unit as VehicleCardBase
+		if selected_unit.capacity == Data.card_data[selected_unit.id]["capacity"]:
+			rotate(selected_unit,OCCUPY_OFFSET_RIGHT,ROTATION_RIGHT)
+	movement.move(selected_unit,tile_position)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
